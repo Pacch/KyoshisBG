001ff721ecc3b06cb84e4cef531397937827d455
Harol Esquivel <harolesquivel92@gmail.com>
2017-09-25 23:07:44 -0500

    update

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,75 @@
-$(".ancla").on("click" , function (e) {
-    var anchor = $(this).attr("href");
+    let consulta = window.matchMedia('(max-width:900px)');
+    consulta.addListener(mediaQuery);
+$( document ).ready(function() {
 
-    $('html,body').animate({
-        scrollTop: $(anchor).offset().top
-    }, 5000);
-    e.preventDefault();
+        $('.seccion-menu').hide();
+        if (consulta.matches) {
+        $('.burguer').on('click', function() {
+            if ($('.burguer').hasClass('fa-bars')) {
+                $('.seccion-menu').show();
+                $('.burguer').removeClass('fa-bars');
+                $('.burguer').addClass('fa-times');
+            }else {
+            $('.seccion-menu').hide();
+            $('.burguer').removeClass('fa-times');
+            $('.burguer').addClass('fa-bars');
+
+            }       
+            });
+        }else{
+            $( ".seccion-menu" ).show();
+
+        }  
 });
+
+// $(document).ready(function(){
+//     $( ".seccion-menu" ).hide();
+//     $('#burguer').on('click', function() {
+//         if ($('.seccion-menu').is(':hidden')) {
+//            $('.seccion-menu').show();
+//            $('#burguer').removeClass('fa-bars');
+//            $('#burguer').addClass('fa-times');
+           
+//         }
+//         else {
+//             $('.seccion-menu').hide();
+//             $('#burguer').removeClass('fa-times');
+//             $('#burguer').addClass('fa-bars');
+//         }       
+//     });
+    
+//     $('.#burguer').on('click', function() {
+//         if ($(this).hasClass('fa-bars')) {
+//            $('.seccion-menu').show();
+//            $(this).removeClass('fa-bars');
+//            $(this).addClass('fa-times');
+//         }
+//         else {
+//             $('.seccion-menu').hide();
+//             $(this).removeClass('fa-times');
+//             $(this).addClass('fa-bars');
+//         }       
+//     });
+// });
+
+// $( document ).ready(function() {
+
+// $( ".cross" ).hide();
+// $( ".menu" ).hide();
+
+
+// $( ".hamburger" ).click(function() {
+// $( ".menu" ).slideToggle( "slow", function() {
+// $( ".hamburger" ).hide();
+// $( ".cross" ).show();
+// });
+// });
+
+// $( ".cross" ).click(function() {
+// $( ".menu" ).slideToggle( "slow", function() {
+// $( ".cross" ).hide();
+// $( ".hamburger" ).show();
+// });
+// });
+
+// });
