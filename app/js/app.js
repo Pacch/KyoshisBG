bb771b963110caf323548e2734651b92a296080c
Harol Esquivel <harolesquivel92@gmail.com>
2017-09-25 23:11:07 -0500

    jquery

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
-    let consulta = window.matchMedia('(max-width:900px)');
-    consulta.addListener(mediaQuery);
-$( document ).ready(function() {
+var consulta = window.matchMedia('(max-width:900px)');
+consulta.addListener(mediaQuery);
 
+function mediaQuery() {
         $('.seccion-menu').hide();
         if (consulta.matches) {
         $('.burguer').on('click', function() {
@@ -18,58 +18,6 @@ $( document ).ready(function() {
             });
         }else{
             $( ".seccion-menu" ).show();
-
         }  
-});
-
-// $(document).ready(function(){
-//     $( ".seccion-menu" ).hide();
-//     $('#burguer').on('click', function() {
-//         if ($('.seccion-menu').is(':hidden')) {
-//            $('.seccion-menu').show();
-//            $('#burguer').removeClass('fa-bars');
-//            $('#burguer').addClass('fa-times');
-           
-//         }
-//         else {
-//             $('.seccion-menu').hide();
-//             $('#burguer').removeClass('fa-times');
-//             $('#burguer').addClass('fa-bars');
-//         }       
-//     });
-    
-//     $('.#burguer').on('click', function() {
-//         if ($(this).hasClass('fa-bars')) {
-//            $('.seccion-menu').show();
-//            $(this).removeClass('fa-bars');
-//            $(this).addClass('fa-times');
-//         }
-//         else {
-//             $('.seccion-menu').hide();
-//             $(this).removeClass('fa-times');
-//             $(this).addClass('fa-bars');
-//         }       
-//     });
-// });
-
-// $( document ).ready(function() {
-
-// $( ".cross" ).hide();
-// $( ".menu" ).hide();
-
-
-// $( ".hamburger" ).click(function() {
-// $( ".menu" ).slideToggle( "slow", function() {
-// $( ".hamburger" ).hide();
-// $( ".cross" ).show();
-// });
-// });
-
-// $( ".cross" ).click(function() {
-// $( ".menu" ).slideToggle( "slow", function() {
-// $( ".cross" ).hide();
-// $( ".hamburger" ).show();
-// });
-// });
-
-// });
+    }
+mediaQuery();
